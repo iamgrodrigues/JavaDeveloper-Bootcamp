9a55e3f8f5fe7e0381b36524c60804e6c1cadba6
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-17 17:06:27 -0300

    add StreamApiExercise 'undone'

--- CollectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
+++ CollectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
@@ -1,19 +1,8 @@
 package one.digitalinovation.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StreamApiExercise {
 
     public static void main(String[] args) {
 
-        List<Friend> friends = new ArrayList<>();
-
-        friends.add(new Friend("Matt", 32, "Persian"));
-        friends.add(new Friend("Leonardo", 35, "Brazilian"));
-        friends.add(new Friend("Shuhey", 30, "Japanese"));
-        friends.add(new Friend("Mika", 40, "Persian"));
-        friends.add(new Friend("Matt", 35, "Persian"));
-        friends.add(new Friend("Matt", 35, "Persian"));
     }
 }
