2bf4636883f55a19d6876a90ea999f4a8388ff20
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-18 12:02:24 -0300

    Update basicJavaDevelopment folder according to new .gitignore file

---
+++ basicJavaDevelopment/createJavaProject/gradle-example/src/test/java/br/com/globallabs/gradleexample/GradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.gradleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
