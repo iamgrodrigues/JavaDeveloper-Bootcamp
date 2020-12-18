15020ab148251e52a798d6659ed0043a4b938f47
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-18 11:37:41 -0300

    Update basicJavaDevelopment folder according to new .gitignore

---
+++ basicJavaDevelopment/createJavaProject/maven-example/src/main/java/br/com/globallabs/mavenexample/MavenExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.mavenexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenExampleApplication.class, args);
+	}
+
+}
