ee96d60379c372d9ded63719d576ed44e332d901
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-21 09:48:18 -0300

    add new lecture

---
+++ advancedJavaDevelopment/src/main/java/functionalParadigm/ImperativeExample.java
@@ -0,0 +1,12 @@
+package functionalParadigm;
+
+public class ImperativeExample {
+
+    public static void main(String[] args) {
+        int value = 10;
+        int result = value * 3;
+        System.out.println("The result is :: "+result);
+    }
+}
+
+// On the Imperative Paradigm the instruction gets executed at the moment of its declaration.
