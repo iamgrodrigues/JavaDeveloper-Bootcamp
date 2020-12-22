62c5cfb18dd29ad06cc4a2e4c61d89cf1eaa4530
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-22 00:37:53 -0300

    Add Stream Exercise

--- collectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
+++ collectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
@@ -1,8 +1,17 @@
 package one.digitalinovation.stream;
 
-public class StreamApiExercise {
+import one.digitalinovation.comparators.Student;
+
+public class StreamApiExercise extends Student {
+
+    public StreamApiExercise(String name, Integer age) {
+        super(name, age);
+    }
 
     public static void main(String[] args) {
+//        TreeMap<Student> students = () -> new Student("Guilherme", 30);
+
+
 
     }
 }
