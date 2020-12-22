3a9eb8bb3c0167e6e499c586a385250a754ca681
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-22 15:16:15 -0300

    Update better way to solve the exercise

--- collectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
+++ collectionsAndStreams/src/main/java/one/digitalinovation/stream/StreamApiExercise.java
@@ -21,24 +21,28 @@ public class StreamApiExercise {
         System.out.println("How many students are in the list? "
                 + students.stream().count());
 
+        System.out.println("Students that are 18+ years old: ");
         students.stream().filter(student -> student.getAge()>18)
-                .forEach(student -> System.out.println("Students that are 18+ years old: "
-                        + student.getName()));
+                .forEach(student -> System.out.println(student.getName()));
+
+        System.out.println("Students that are 18+ years old on a separated list: "
+                + students.stream().filter(student -> student.getAge()>18)
+                .map(student -> student.getName()).collect(Collectors.toList()));
 
         System.out.println("What are the students with the letter 'b' on their names? "
-                + students.stream().filter(student -> student.getName().toLowerCase().contains("b")).collect(Collectors.toList()));
-
+                + students.stream().filter(student -> student.getName().toLowerCase().contains("b"))
+                .map(student -> student.getName()).collect(Collectors.toList()));
 
         System.out.println("Is there any name that contains the letter 'd'? "
                 + students.stream().anyMatch((student) -> student.getName().contains("d")));
 
-        System.out.println("What is the oldest student in the list? " + students.stream()
-                .max(Comparator.comparing(student -> student.getAge()))
-                .stream().collect(Collectors.toList()));
+        System.out.println("What is the oldest student in the list? "
+                + students.stream().max(Comparator.comparing(student -> student.getAge()))
+                .get().getName());
 
-        System.out.println("What is the youngest student in the list? " + students.stream()
-                .min(Comparator.comparing(student -> student.getAge()))
-                .stream().collect(Collectors.toList()));
+        System.out.println("The youngest student in a new the list? "
+                + students.stream().min(Comparator.comparing(student -> student.getAge()))
+                .stream().map(student -> student.getName()).collect(Collectors.toList()));
 
 //        var names = students.stream().map(student -> {
 //            System.out.println(student.getName());
