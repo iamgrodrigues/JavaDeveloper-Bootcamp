10e4344db96970619bd0d2ab15fdc5c70e6fc850
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-24 17:33:37 -0300

    add Collections jdk-11 example file

---
+++ advancedJavaDevelopment/src/main/java/Collections/Example.java
@@ -0,0 +1,12 @@
+package Collections;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        Collection<String> names = List.of("Guilherme", "Taisa", "Cristina", "Rodrigues");
+
+        System.out.println(names);
+    }
+}
