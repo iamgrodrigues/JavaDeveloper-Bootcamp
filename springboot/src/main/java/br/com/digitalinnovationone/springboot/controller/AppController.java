5c42dce8432608cdccf889649a4f70d8a6594f21
iamgrodrigues <iamgrodrigues@gmail.com>
2020-12-31 15:25:03 -0300

    Update adding Environment variables

--- springboot/src/main/java/br/com/digitalinnovationone/springboot/controller/AppController.java
+++ springboot/src/main/java/br/com/digitalinnovationone/springboot/controller/AppController.java
@@ -10,8 +10,16 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NONE}")
+    private String dbEnvironmentVariable;
+
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable() {
+        return "The following variable was set to: " + dbEnvironmentVariable;
     }
 }
